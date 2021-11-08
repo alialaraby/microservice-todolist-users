2f2a9421ec4821a082b9f1e72e82345582f0ff9b
ali alaraby <alisakralaraby@gmail.com>
2021-11-08 14:55:23 +0200

    added user-actions schema model

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 //calling the initial startup modules
-require('./startup/db')();
+require('./startup/db').connect();
 require('./startup/port')(app);
 require('./startup/routing')(app);
--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,15 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelizeConnection = new Sequelize('todolist_users', 'postgres', '0000', {
+    host: 'localhost', dialect: 'postgres'
+});
+require('../models/user-actions')(sequelizeConnection, DataTypes);
 
-module.exports = () => {
-    const sequelize = new Sequelize('todolist_users', 'postgres', '0000', {
-        host: 'localhost', dialect: 'postgres'
-    });
+connect = () => {
     try {
-        sequelize.authenticate();
+        sequelizeConnection.sync();
         console.log('connected to db');
     } catch (error) {
         console.log('something went wrong: ', error);
     }
 }
+module.exports = { connect: connect, sequelize: sequelizeConnection }
