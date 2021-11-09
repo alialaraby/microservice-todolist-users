7fd54f4f273444b75550cf6d93b6aa41b971290a
ali alaraby <alisakralaraby@gmail.com>
2021-11-09 10:36:47 +0200

    added config folder

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,7 @@
+const config = require('config');
 const { Sequelize, DataTypes } = require('sequelize');
-const sequelizeConnection = new Sequelize('todolist_users', 'postgres', '0000', {
-    host: 'localhost', dialect: 'postgres', logging: false
+const sequelizeConnection = new Sequelize(config.get('db_name'), config.get('db_username'), config.get('db_password'), {
+    host: config.get('db_host'), dialect: config.get('db_dialect'), logging: config.get('db_enable_logging')
 });
 const UserActions = require('../models/user-actions')(sequelizeConnection, DataTypes);
 const User = require('../models/user')(sequelizeConnection, DataTypes);
@@ -15,12 +16,12 @@ UserActions.hasMany(UserHistory);
 UserHistory.belongsTo(UserActions);
 
 //inserting some seed values
-const userSeeds = require('../seed-queries/users');
-const userActionsSeeds = require('../seed-queries/user-actions');
-const userHistorySeeds = require('../seed-queries/user-history');
+// const userSeeds = require('../seed-queries/users');
+// const userActionsSeeds = require('../seed-queries/user-actions');
+// const userHistorySeeds = require('../seed-queries/user-history');
 // userSeeds.insertUser(User);
 // userActionsSeeds.insertUserActions(UserActions);
-userHistorySeeds.insertUserHistory(UserHistory);
+// userHistorySeeds.insertUserHistory(UserHistory);
 
 connect = () => {
     try {
